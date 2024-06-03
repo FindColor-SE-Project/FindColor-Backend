b8231291950837b7174f853b4291ace3f9135e73
Areeya <amyschclass@gmail.com>
2024-06-03 21:08:50 +0700

    get brand and image link

--- importExcel.py
+++ importExcel.py
@@ -1,39 +1,63 @@
 import pandas as pd
 import openpyxl
 import re
+from bs4 import BeautifulSoup
 
 file = r"Scrape/4U2/4U2Lip.xlsx"
 df = pd.read_excel(file)
 
 # ดึงข้อมูลจากชื่อ column
-exCategory = list(df['Category_Brand'])
+exCategory_Brand = list(df['Category_Brand'])
 exLogo = list(df['Logo-src'])
 exImage = list(df['img-src'])
-exColor = list(df['color'])
 exDescription = list(df['Description'])
 
 
-# แก้ข้อมูลของ exCategory ให้เป็น (Lips, Blush หรือ Eyeshadow)
-for i, element in enumerate(exCategory):
+# หาและแก้ข้อมูลของ exCategory ให้เป็น (Lips, Blush หรือ Eyeshadow)
+exCategory = []
+for element in exCategory_Brand:
     if "ปาก" in element:
-        exCategory[i] = "Lips"
+        exCategory.append("Lips")
     elif "แก้ม" in element:
-        exCategory[i] = "Blush"
+        exCategory.append("Blush")
     elif "ตา" in element:
-        exCategory[i] = "Eyeshadow"
+        exCategory.append("Eyeshadow")
     else:
         print(element)
+
+
+# หาแบรนด์ใน exCategory_Brand และรวบรวมไว้ใน exBrand
+exBrand = []
+brands = ['2P', '4U2', 'CathyDoll', 'Laka', 'Sasi']
+for element in exCategory_Brand:
+    found_brand = None
+    for brand in brands:
+        if brand in element:
+            found_brand = brand
+            break
+    if found_brand:
+        exBrand.append(found_brand)
+    else:
+        exBrand.append(None)  # หรือสามารถใช้เป็น "" ได้ถ้าต้องการ
 
 
 # แยก name ออกจาก description
 exName = []
 for description in exDescription:
-    match = re.search(r'^([A-Za-z0-9\s#.,&()-]+)', description)
-    if match:
-        exName.append(match.group(1).strip())
-    else:
-        exName.append("")
+    split_description = re.split(r'[\u0E00-\u0E7F]', description, maxsplit=1)
+    exName.append(split_description[0].strip())
 
+
+# ดึง link รูปสีจาก color column
+color_column = df['color']
+exColor = []
+for html_content in color_column:
+    soup = BeautifulSoup(html_content, 'html.parser')
+    active_image_links = [img['src'] for li in soup.find_all('li', class_='active') for img in li.find_all('img')]
+    exColor.append(active_image_links)
+
+
+print("Categorized exCategory:", exCategory)
+print("Found Brands:", exBrand)
+print("Extracted Image Links from 'active' class:", exColor)
 print("Extracted Names:", exName)
-
-# ต้องแก้ exColor
