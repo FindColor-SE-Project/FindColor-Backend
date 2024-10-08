d7438ce513adce4f3fc7be5896e8e39e69da1272
BT <kornkanok_kanchana@cmu.ac.th>
2024-10-08 12:01:43 +0700

    Add filename column in database

--- backend/models/ImageModel.py
+++ backend/models/ImageModel.py
@@ -2,8 +2,8 @@ from backend.database.Database import db
 
 
 class Image(db.Model):
-    __bind_keys__ = 'images'
     __tablename__ = 'images'
     id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String(255), nullable=False)
     filepath = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
