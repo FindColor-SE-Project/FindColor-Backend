0b08f092e4628620bcf18f6dc668107e6c01927f
BT <kornkanok_kanchana@cmu.ac.th>
2024-10-11 14:04:21 +0700

    Change Type in Model

--- backend/models/ImageModel.py
+++ backend/models/ImageModel.py
@@ -5,5 +5,5 @@ class Image(db.Model):
     __tablename__ = 'images'
     id = db.Column(db.Integer, primary_key=True)
     filename = db.Column(db.String(255), nullable=False)
-    filepath = db.Column(db.String(255), nullable=False)
-    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    filepath = db.Column(db.LONGBLOB, nullable=False)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp()) #เวลาที่ไฟล์ถูกอัปโหลด
