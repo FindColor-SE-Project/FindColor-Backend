0039f9d3936a9c375a389615ee9085ccdb6889d4
BT <kornkanok_kanchana@cmu.ac.th>
2024-11-04 02:42:14 +0700

    Fix Error

--- backend/__init__.py
+++
@@ -1,2 +0,0 @@
-# backend/__init__.py
-# This file can be empty or can contain initialization code if needed.
--- backend/importData/ty.py
+++
@@ -1,4 +0,0 @@
-import backend.importData.get_all_product as allP
-
-product = allP.get_product()
-print(product)
